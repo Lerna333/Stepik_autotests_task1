981020861d7d1fca20079ba9b17fab666a6bf61e
SSSvieta <148636409+SSSvieta@users.noreply.github.com>
2025-01-06 14:35:14 +0100

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,1 +1,12 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_button_exist(browser):
+    browser.get(link)
+    button = browser.find_element(By.XPATH, "//button[contains(@class, 'btn-add-to-basket')]")
+    assert button is not None, "Button with class 'btn-add-to-basket' not found on the page"
 
